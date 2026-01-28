e6910ce6acf8baffad13cacc1b8b03320abfd126
luusytruong <lst27062004@gmail.com>
2026-01-28 15:07:41 +0700

    feat: update scripts, fontend domain

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ["chat.truong.cloud"],
+    allowedHosts: ["chat.truong.cloud", "tea.truong.cloud"],
     proxy: {
       "/api": {
         target: "http://localhost:3001",
